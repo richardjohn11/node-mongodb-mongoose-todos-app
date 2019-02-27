45c0d6baddad78759cd733d58f1e545d948c2f18
richard john garcia <richardjohngarcia@gmail.com>
2019-02-26 21:51:04 -0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,18 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
--- server/server.js
+++ server/server.js
@@ -139,8 +139,21 @@ app.post("/users/login", (req, res) => {
     });
 });
 
+//Me/Profile page
 app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//Logout
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(PORT, () => {
