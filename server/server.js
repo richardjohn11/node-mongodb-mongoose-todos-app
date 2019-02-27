1d547d83141e76f579be00eb6a62d666873565e2
richard john garcia <richardjohngarcia@gmail.com>
2019-02-26 21:34:11 -0700

    Add POST /users/login ROUTE

--- server/server.js
+++ server/server.js
@@ -124,6 +124,21 @@ app.post("/users", (req, res) => {
     .catch(e => res.status(400).send(e));
 });
 
+//POST user login
+app.post("/users/login", (req, res) => {
+  const body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      user.generateAuthToken().then(token => {
+        res.header("x-auth", token).send(user);
+      });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
 app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
